755c9f948bca4f228933e37be3e4adf2c9ca0720
a7medmohamed116 <ahmedali1282727@gmail.com>
2026-07-30 16:32:00 +0300

    feat : add  create  payment intent endpoint

---
+++ ECommerce.API/Controllers/PaymentController.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Application.Contracts;
+using ECommerce.Application.DTOs.BasketDTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    public class PaymentController : ApiBaseController
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [Authorize]
+        [HttpPost("{basketId}")]
+        public async Task<ActionResult<BasketDto>> CreateOrUpdatePaymentIntent(string basketId ,CancellationToken ct = default)
+        {
+            var result = await _paymentService.CreateOrUpdatePaymentIntent(basketId, ct);
+            return ToActionResult(result);
+
+        }   
+    }
+}
